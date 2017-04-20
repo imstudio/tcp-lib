f3802d0859989be72e7a1f0e64db383a5103cd3d
tecle@126.com <tecle@126.com>
2017-04-20 18:12:39 +0800

    add etc.

---
+++ etc/lab/main.cpp
@@ -0,0 +1,13 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <thread>
+#include "concurrentqueue.h"
+
+
+int main(){
+    ConcurrentQueue<int> queue(1000);
+}
